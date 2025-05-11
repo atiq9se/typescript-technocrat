3fe4c27f9ab7fb9a3ec6893a68c8334bcc4564b8
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-05-11 23:13:22 +0600

    Encapsulation in OOP

---
+++ module3/3.10.ts
@@ -0,0 +1,38 @@
+{
+    // access modifiers
+    class BankAccount{
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+        
+        constructor(id: number, name: string, balance: number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+        addDeposit(amount: number){
+           this._balance = this._balance + amount
+        }
+        private getBalance(){
+            return this._balance;
+        }
+
+        getHiddenMethod(){
+            return this.getBalance();
+        }
+    }
+
+    class StudentAccount extends BankAccount{
+        test(){
+            this._balance
+        }
+    }
+
+    const goribManusherAccount = new BankAccount(111, "mr.gorib", 20);
+    // goribManusherAccount.balance = 0;
+    goribManusherAccount.addDeposit(0);
+    const mybalance = goribManusherAccount.getBalance();
+    console.log(mybalance)
+
+    //
+}
